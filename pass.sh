2693eb691ce603a9b4eeba8f6b0333b2a2a7a033
collin <lijun877@gmail.com>
2012-03-12 15:33:07 +0800

    some scripts

---
+++ pass.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+password="test_passwd"
+username="test_user"
+useradd $username
+expect -c "
+        set timeout 5;
+        spawn passwd test
+        expect \"UNIX password:\" { send \"$password\r\";};
+        sleep 2;
+        expect \"UNIX password:\" { send \"$password\r\";};
+        expect eof;"
+
---
+++ sendsms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+all="$*"
+msg=`echo $all |cut -d' ' -f2-`
+/root/bin/fetion/fetion --mobile=13817201301 --pwd=sdsf@2011 --to=$1 --msg-utf8="$msg"
